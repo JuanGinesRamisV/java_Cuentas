ad1520e48169de7589a9140c388a83f4e1c92e4e
juan gines ramis vivancos <jramisv@cifpfbmoll.eu>
2020-01-15 22:49:49 +0100

    ejercicio 6

--- Java_Primeros_ejercicios/src/java_primeros_ejercicios/ejercicio6.java
+++ Java_Primeros_ejercicios/src/java_primeros_ejercicios/ejercicio6.java
@@ -12,23 +12,44 @@ import java.util.Scanner;
  * @author juang
  */
 public class ejercicio6 {
+
     public static void main(String[] args) {
-        int numeros[]= new int [12];
-        int numeros1[]=new int [12];
-        int numerosfinal[]=new int[24];
-        Scanner scanner= new Scanner(System.in);
+        // TODO code application logic here
+        int numeros[] = new int[12];
+        int numeros1[] = new int[12];
+        int listafinal[] = new int[24];
         int i;
-        int aux=0;
-        System.out.println("introduce los 12 primeros numeros");
-        for(i=0;i<numeros.length;i++){
-            numeros[i]=scanner.nextInt();
+        int aux = 0;
+        int auxlista = 0;
+        int auxlista1 = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("introduce los elementos de la primera lista");
+        for (i = 0; i < numeros.length; i++) {
+            numeros[i] = scanner.nextInt();
         }
-        System.out.println("Introduce los 12 ultimos numeros");
-        for(i=0;i<numeros1.length;i++){
-            numeros1[i]=scanner.nextInt();
+        System.out.println("introduce los elementos de la segunda lista");
+        for (i = 0; i < numeros1.length; i++) {
+            numeros1[i] = scanner.nextInt();
         }
-        for(i=0;i<numeros.length;i=i+3){
-            System.out.println(i);
+        //for para añadir los elementos a la lista final//
+        for (i = 0; i < listafinal.length; i += 3) {
+            if (i % 2 == 0) {
+                aux = i;
+                listafinal[i] = numeros[auxlista];
+                listafinal[aux += 1] = numeros[auxlista += 1];
+                listafinal[aux += 1] = numeros[auxlista += 1];
+            } else if (i % 2 != 0) {
+                aux = i;
+                listafinal[i] = numeros1[auxlista1];
+                listafinal[aux += 1] = numeros1[auxlista1 += 1];
+                listafinal[aux += 1] = numeros1[auxlista1 += 1];
+            }
         }
+        System.out.println("----------------");
+        for (i = 0; i < listafinal.length; i++) {
+            System.out.println(listafinal[i]);
+        }
+
     }
 }
