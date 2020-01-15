4e01c0b8d65cc492250367bb6df608aadec4d6b6
juan gines ramis vivancos <jramisv@cifpfbmoll.eu>
2020-01-15 17:56:15 +0100

    ejercicio 5

--- Java_Primeros_ejercicios/src/java_primeros_ejercicios/prueba.java
+++ Java_Primeros_ejercicios/src/java_primeros_ejercicios/prueba.java
@@ -11,24 +11,27 @@ import java.util.Scanner;
  *
  * @author juang
  */
-public class ejercicio6 {
+public class prueba {
     public static void main(String[] args) {
-        int numeros[]= new int [12];
-        int numeros1[]=new int [12];
-        int numerosfinal[]=new int[24];
-        Scanner scanner= new Scanner(System.in);
         int i;
-        int aux=0;
-        System.out.println("introduce los 12 primeros numeros");
+        int numeros[] = new int[12];
+        int numeros1[] = new int[12];
+        int numerosFinal[] = new int[numeros.length*2];
+        int aux;
+        Scanner scanner = new Scanner (System.in);
         for(i=0;i<numeros.length;i++){
-            numeros[i]=scanner.nextInt();
+            numeros[i] = scanner.nextInt();
         }
-        System.out.println("Introduce los 12 ultimos numeros");
         for(i=0;i<numeros1.length;i++){
             numeros1[i]=scanner.nextInt();
         }
-        for(i=0;i<numeros.length;i=i+3){
+        for(i=0;i<numeros.length;i+=3){
+            aux=i+1;
             System.out.println(i);
+            System.out.println(aux);
+            aux++;
+            System.out.println(aux);
+
         }
     }
 }
